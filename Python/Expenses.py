8f85095db4f5dfca812110766560119831b9e8be
rene bakx <r.bakx@fontys.nl>
2023-04-02 20:56:07 +0200

    Update Expenses.py

--- Python/Expenses.py
+++ Python/Expenses.py
@@ -16,65 +16,60 @@ num_days = (end_date - start_date).days
 print(f'Number of days between {start_date_str} and {end_date_str}: {num_days}')
 AVERAGE_LENGTH_MOMTH = 30.44
 
-categories = ["food", "non-food", "inboedel", "gas-water-licht", "auto", "huisdieren", "abonnementen", "zorgverzekering"]
-
 # Define the rows to select
-rows_to_select = {
-    "AH": {"category" : "food", "string": "AH"},
-    "Albert Heijn": {"category" : "food", "string": "Albert Heijn"},
-    "Jumbo": {"category" : "food", "string": "Jumbo"},
-    "Lidl": {"category" : "food", "string": "Lidl"},
-    "Sligro": {"category" : "food", "string": "Sligro"},
-    "TooGoodToGo": {"category" : "food", "string": "TooGoodToGo"},
-    "Ekoplaza": {"category" : "food", "string": "Ekoplaza"},
-    "IZMIR": {"category" : "food", "string": "IZMIR"},
-    "NETTORAMA": {"category" : "food", "string": "NETTORAMA"},
-    "Action": {"category" : "non-food", "string": "Action"},
-    "Kruidvat": {"category" : "non-food", "string": "Kruidvat"},
-    "RitualsCosmetics": {"category" : "non-food", "string": "RitualsCosmetics"},
-    "TINQ": {"category": "auto", "string": "TINQ"},
-    "Pierre Beelen": {"category": "huisdieren", "string": "Pierre Beelen"},
-    "Praxis": {"category": "inboedel", "string": "Praxis"},
-    "Zilveren Kruis": {"category": "zorgverzekering", "string": "Zilveren Kruis"},
-    "ANWB Energie": {"category": "gas-water-licht", "string": "ANWB Energie"},
-    "Vandebron": {"category": "gas-water-licht", "string": "Vandebron"},
-    "PLINQ": {"category": "abonnementen", "string": "PLINQ"},
+categories_to_select = {
+    "food": {"description": "^AH|^Albert Heijn|^Jumbo|^Lidl|^Sligro|^Ekoplaza|^IZMIR|^NETTORAMA|^TooGoodToGo"},
+    "non-food": {"description": "^Action|^Kruidvat|^RitualsCosmetics"},
+    "auto": {"description": "^TINQ"},
+    "huisdieren": {"description": "^Pierre Beelen"},
+    "inboedel": {"description": "^Praxis"},
+    "zorgverzekering": {"description": "^Zilveren Kruis"},
+    "gas-water-licht": {"description": "^ANWB Energie|^Vandebron"},
+    "abonnementen": {"description": "^PLINQ|^DPG MEDIA BV"},
+    "vakantie-uitjes": {"description": "Stayokay"},
 }
 
+category_to_ignore = "^ING Bank"
+
 # Initialize selected_rows to empty DataFrames for each row_name
-category_rows = {category: pd.DataFrame() for category in categories}
+category_rows = {category_name: pd.DataFrame() for category_name, _ in categories_to_select.items()}
 
 # Filter the rows and compute the total amounts
-for row_name, row_data in rows_to_select.items():
-    category_df = df[df["Naam / Omschrijving"].str.startswith(row_data["string"])]
+for category_name, category_data in categories_to_select.items():
+    category_df = df[df["Naam / Omschrijving"].str.contains(category_data["description"], regex=True)]
     # Get the "Bij Af" column for the selected rows
     bij_af = category_df["Af Bij"].apply(lambda x: 1 if x == "Af" else -1)
     # Multiply the "Bedrag (EUR)" column by the "Af Bij" column
     category_df["Bedrag (EUR)"] = bij_af * category_df["Bedrag (EUR)"].astype(float)
-    category_rows[row_data["category"]] = pd.concat([category_rows[row_data["category"]], category_df])
+    category_rows[category_name] = pd.concat([category_rows[category_name], category_df])
 
 # Initialize total_amounts to zero for each row_name
-total_amounts = {category: 0.0 for category in categories}
+total_amounts = {category: 0.0 for category, row_data in categories_to_select.items()}
 # Calculate total_amounts and print the selected rows and total amounts
 total_amount_all = 0.0
 print("****************************************")
-for row_name in categories:
-    category_df = category_rows[row_name]
+for category_name, _ in categories_to_select.items():
+    category_df = category_rows[category_name]
     if not category_df.empty:
-        total_amounts[row_name] = category_rows[row_name]["Bedrag (EUR)"].astype(float).sum()
-        total_amount_all += total_amounts[row_name]
-        print(f"Monthly amount for {row_name}: €{total_amounts[row_name] / AVERAGE_LENGTH_MOMTH:.2f}")
+        total_amounts[category_name] = category_rows[category_name]["Bedrag (EUR)"].astype(float).sum()
+        total_amount_all += total_amounts[category_name]
+        print(f"Monthly amount for {category_name}: €{total_amounts[category_name] / AVERAGE_LENGTH_MOMTH:.2f}")
 print(f"TOTAL PER MONTH: €{total_amount_all / AVERAGE_LENGTH_MOMTH:.2f}")
 print("****************************************")
     
 # Compute the unselected rows and count their amount
 selected_indexes = pd.Index([])
-for row_name, row_data in category_rows.items():
-    selected_indexes = selected_indexes.union(row_data.index)
-unselected_rows = df.drop(selected_indexes)
-num_unselected_rows = len(unselected_rows)
+for _, category_data in category_rows.items():
+    selected_indexes = selected_indexes.union(category_data.index)
+unhandled_rows = df.drop(selected_indexes)
+
+# Exclude rows matching the regular expressions in categories_to_select
+for _, category_data in category_rows.items():
+    unhandled_rows = unhandled_rows[~unhandled_rows['Naam / Omschrijving'].str.contains(category_to_ignore, regex=True)]
+
+num_unhandled_rows = len(unhandled_rows)
 
 # Print the unselected rows and their count
-print(f"Number of unselected rows: {num_unselected_rows}")
-print("Unselected rows:")
-print(unselected_rows)
+print(f"Number of unhandled rows: {num_unhandled_rows}")
+print("Unhandled rows:")
+print(unhandled_rows)
