56574617a42c5c14d4c22b0b030082bd781cf277
rene bakx <r.bakx@fontys.nl>
2023-04-09 20:32:40 +0200

    Update Expenses.py

--- Python/Expenses.py
+++ Python/Expenses.py
@@ -14,34 +14,44 @@ end_date = datetime.strptime(end_date_str, '%Y%m%d')
 num_days = (end_date - start_date).days
 # Print the number of days between the start and end dates
 print(f'Number of days between {start_date_str} and {end_date_str}: {num_days}')
-AVERAGE_LENGTH_MOMTH = 30.44
+AVERAGE_LENGTH_MONTH = 30.44
 
-# Define the rows to select
+columns_to_check = ["Naam / Omschrijving", "Mededelingen"]
+
+# Define the categories to select
 categories_to_select = {
-    "food": {"description": "^AH|^Albert Heijn|^Jumbo|^Lidl|^Sligro|^Ekoplaza|^IZMIR|^NETTORAMA|^TooGoodToGo"},
-    "non-food": {"description": "^Action|^Kruidvat|^RitualsCosmetics"},
-    "auto": {"description": "^TINQ"},
-    "huisdieren": {"description": "^Pierre Beelen"},
-    "inboedel": {"description": "^Praxis"},
-    "zorgverzekering": {"description": "^Zilveren Kruis"},
-    "gas-water-licht": {"description": "^ANWB Energie|^Vandebron"},
-    "abonnementen": {"description": "^PLINQ|^DPG MEDIA BV"},
-    "vakantie-uitjes": {"description": "Stayokay"},
+    "food": "^AH|Albert Heijn|Jumbo|Lidl|Sligro|Ekoplaza|Hema|IZMIR|NETTORAMA|TooGoodToGo|VieCuri|Maxima MC|Rudolf Vermeulen|De Genneper Hoeve|SumUp|INTERMARCHE|Restaurant|RISTORANTE",
+    "non-food": "Action|Kruidvat|RitualsCosmetics|Praxis|IKEA|Amazon|bol\.com|Bershka|Lucardi|Favoloso|HOD Electronics|Conrad|Eleshop|Muco Amsterdam|Bloemerie|Wehkamp",
+    "tikkie": "Tikkie|Betaalverzoek",
+    "auto": "GARAGE VAN OOL|TINQ|^BP|KAMPEN ASSURANTIE|MijnAutoOnderdelen|Interparking|ParkBee|79-KDL-1",
+    "huisdieren": "Pierre Beelen",
+    "woning": "BRABANT WATER NV|Gemeente Waalre|hypotheek|van Hest",
+    "inboedel": "Praxis",
+    "zorgverzekering": "Zilveren Kruis|Infomedics|Apotheek",
+    "verzekeringen woning": "FBTO",
+    "gas-water-licht": "ANWB Energie|^Vandebron",
+    "abonnementen": "PLINQ|DPG MEDIA BV|Simyo|MICROSOFT|Kosten OranjePakket|Kosten tweede rekeninghouder",
+    "studie": "Stichting Eindhovens Christelijk Vo|Lara Baltessen Omschrijving: Collegegeld|Stichting Avans Omschrijving: Collegegeld",
+    "zakgeld": "kleedgeld|studiegeld|zakgeld",
+    "kinderbijslag": "SVB Roermond|VOORSCHOT KIT/Kgb",
+    "vakantie-uitjes": "Stayokay|Spa Sense|Subway|The Happiness Kitchen|Fifth NRE|CCVSancy Tres|waterreus|Snowworld|Outside Escape|Cafetaria|STATION OUTDOOR RETIE|INCASSO CREDITCARD|Huttopia|Roompot|Parkhoeve de Middelt|CCVBar Potential|CAMP BEAUREGARD",
 }
 
-category_to_ignore = "^ING Bank"
+category_to_ignore = "^ING Bank|STICHTING FONTYS|Stichting Sint Annakloos|Stichting Bitva|BTC Direct Europe"
 
 # Initialize selected_rows to empty DataFrames for each row_name
 category_rows = {category_name: pd.DataFrame() for category_name, _ in categories_to_select.items()}
 
 # Filter the rows and compute the total amounts
-for category_name, category_data in categories_to_select.items():
-    category_df = df[df["Naam / Omschrijving"].str.contains(category_data["description"], regex=True)]
-    # Get the "Bij Af" column for the selected rows
-    bij_af = category_df["Af Bij"].apply(lambda x: 1 if x == "Af" else -1)
-    # Multiply the "Bedrag (EUR)" column by the "Af Bij" column
-    category_df["Bedrag (EUR)"] = bij_af * category_df["Bedrag (EUR)"].astype(float)
-    category_rows[category_name] = pd.concat([category_rows[category_name], category_df])
+for category_name, regexp in categories_to_select.items():
+    for column_name in columns_to_check:
+        # Use copy to prevent modifying the original df, otherwise the 'SettingWithCopyWarning' can occur.
+        category_df = df[df[column_name].str.contains(regexp, case=False, regex=True)].copy()
+        # Get the "Bij Af" column for the selected rows
+        bij_af_column = category_df["Af Bij"].apply(lambda x: 1 if x == "Af" else -1)
+        # Multiply the "Bedrag (EUR)" column by the "Af Bij" column
+        category_df["Bedrag (EUR)"] = bij_af_column * category_df["Bedrag (EUR)"].astype(float)
+        category_rows[category_name] = pd.concat([category_rows[category_name], category_df]).drop_duplicates()
 
 # Initialize total_amounts to zero for each row_name
 total_amounts = {category: 0.0 for category, row_data in categories_to_select.items()}
@@ -53,8 +63,9 @@ for category_name, _ in categories_to_select.items():
     if not category_df.empty:
         total_amounts[category_name] = category_rows[category_name]["Bedrag (EUR)"].astype(float).sum()
         total_amount_all += total_amounts[category_name]
-        print(f"Monthly amount for {category_name}: €{total_amounts[category_name] / AVERAGE_LENGTH_MOMTH:.2f}")
-print(f"TOTAL PER MONTH: €{total_amount_all / AVERAGE_LENGTH_MOMTH:.2f}")
+        print(f"Monthly amount for {category_name}: €{total_amounts[category_name] / (num_days / AVERAGE_LENGTH_MONTH):.2f}")
+print(f"TOTAL PER MONTH: €{total_amount_all / (num_days / AVERAGE_LENGTH_MONTH):.2f}")
+print(f"NUMBER OF MONTHS: {num_days / AVERAGE_LENGTH_MONTH:.2f}")
 print("****************************************")
     
 # Compute the unselected rows and count their amount
