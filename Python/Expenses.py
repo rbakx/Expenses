78398f9cb682fc7ed382dbf5688dcfcc8062fea1
rene bakx <r.bakx@fontys.nl>
2023-04-10 17:07:16 +0200

    Update Expenses.py

--- Python/Expenses.py
+++ Python/Expenses.py
@@ -19,27 +19,30 @@ columns_to_check = ["Naam / Omschrijving", "Mededelingen"]
 
 # Define the categories to select
 categories_to_select = {
-    "food": "Sodexo|BIERENS VOF GEBAK|Miss Bettie|Jan Linders|Mitra|Foodticket|Bakkerij|^AH |Albert Heijn|Jumbo|Lidl|Sligro|Ekoplaza|Hema|IZMIR|Ozturk|NETTORAMA|TooGoodToGo|VieCuri|Maxima MC|Rudolf Vermeulen|De Genneper Hoeve|SumUp|Tonys Chocolonely|INTERMARCHE|Restaurant|RISTORANTE",
-    "non-food": "Patisserie|Xenos|Fashion|STRADIVARIUS|Decathlon|Bijenkorf|Bistro|Mouser Electronic|Wibra|PRIMARK|vanHaren|Jewellery|Kadoshop|Techniparts|Planten|Bever|The Sting|Action|Kruidvat|RitualsCosmetics|Praxis|IKEA|Amazon|bol\.*com|123 3D|Bershka|Bruna|Tochgoedkoopnl|^CA|^HM|Hennes .*Mauritz|Pull.*Bear|Amsterdam SAL FO|Lucardi|Favoloso|Nelson|Waterstones|New York Finest shirts|HOD Electronics|Conrad|Eleshop|Reichelt|Muco Amsterdam|Bloemerie|Wehkamp",
-    "tikkie": "Tikkie|Betaalverzoek",
-    "auto": "Auto|Tankstation|TotalEnergies|EXPRTILBURG|KEIZ3320|TMCPVALKENSWAARD|GARAGE VAN OOL|Shell|TINQ|^BP|TAMOIL|KAMPEN ASSURANTIE|VKG VERZEKERINGEN|MijnAutoOnderdelen|Interparking|ParkBee|Q-Park|QPark|79-KDL-1",
-    "huisdieren": "Pierre Beelen|VDierenspeciaalzaak",
-    "woning": "BRABANT WATER NV|Gemeente Waalre|hypotheek|van Hest",
-    "inboedel": "Planten|Praxis|Matt Sleeps|ABC Webshop|Vloerglijders\.nl|groenovatie|Tuincentrum|Intratuin|vijver",
-    "zorgverzekering": "Zilveren Kruis|Infomedics|Apotheek|Al Dente|Pearle",
-    "verzekeringen woning": "FBTO",
-    "gas-water-licht": "ANWB Energie|^Vandebron",
-    "abonnementen": "Piano|PLINQ|DPG MEDIA|Simyo|MICROSOFT|Kosten OranjePakket|Kosten tweede rekeninghouder",
-    "studie": "Stichting Eindhovens Christelijk Vo|Lara Baltessen Omschrijving: Collegegeld|Stichting Avans Omschrijving: Collegegeld",
-    "zakgeld/gift": "kleedgeld|studiegeld|zakgeld|Theoriecentrum\.nl",
-    "kinderbijslag": "KINDERBIJSLAG|VOORSCHOT KIT/Kgb",
-    "vakantie-uitjes": "ECOLONIE|Koala Express|BROOD2DAY|Brandweerkantin|Horeca|Pathe|Ijsselhallen|Chidoz|LOODS 61|SMAAKVOL|Bedrijfsrest|Sauna|Cafe|Next Nature Network|A'DAM Six Senses|Sherlocked|Bowling|Plein 4|IJsco Fantastico|De Vooruitgang|Avocado Show|Time4t|Hotel|tourist|Stayokay|Conscious Westerpark|Moon AMSTERDAM|Majestic Amsterdam|Spa Sense|Subway|The Happiness Kitchen|Fifth NRE|CCVSancy Tres|waterreus|t Goude Hooft|Belicio Cheats|Soil Of Amsterdam|Kottmann en AMSTERDAM|alpenpark|Snowworld|Outside Escape|TicketCounter|Cafetaria|STATION OUTDOOR RETIE|INCASSO CREDITCARD|Huttopia|Roompot|Parkhoeve de Middelt|CCVBar Potential|CAMP BEAUREGARD|A J M de Bruijn|Bolier|Erik|Paasen|Venbrux|AGG JANSSEN|Saskia",
-    "Gift": "Greenpeace|Rop",
+    "Food": "2Bruder|Groente Fruit|Food|Pindakaaswinkel|Too Good To Go|Bakker Bart|Chocolaterie|van Heeswijk Eersel|Patisserie|Xaris Lunet Zorg|Sodexo|BIERENS VOF GEBAK|Miss Bettie|Jan Linders|Mitra|Bakkerij|^AH |Albert Heijn|Jumbo|Lidl|Aldi|Sligro|Ekoplaza|Hema|IZMIR|Ozturk|NETTORAMA|TooGoodToGo|VieCuri|Maxima MC|Rudolf Vermeulen|De Genneper Hoeve|SumUp|Tonys Chocolonely|INTERMARCHE",
+    "Non-food": "Dragon Tattoo|Van alles en meer|Secrid|Holland.*Barrett|Perry Sport|Zalando|Interoffice|Intersport|De Paskamer|Etos|Intelbazaar|Schoen|Travelbags|Hunkemoeller|Smartphonehoesjes|YourSurprise|Coolblue|Stichting Kringloo|Dikke Mik|HORNBACH|PANDORA|ONLY |ZEEMAN|Timco|clothing|Clay And Glow|Costes Eindhoven|MINISO|Parfumerie|Cosmetics|Xenos|Fashion|STRADIVARIUS|Decathlon|Bijenkorf|Bistro|Wibra|PRIMARK|vanHaren|Jewellery|Kadoshop|Planten|Bever|The Sting|Action|Kruidvat|RitualsCosmetics|Praxis|IKEA|Amazon|bol\.*com|Bershka|Bruna|Tochgoedkoopnl|^CA|^HM|Hennes .*Mauritz|Pull.*Bear|Amsterdam SAL FO|Lucardi|Favoloso|Nelson|New York Finest shirts|Muco Amsterdam|Bloemerie|Wehkamp",
+    "Hobby": "Boekhandel|Waterstones|Paagman|Keyboardcentrum|Zingende Snaar|STRIPSPECIALIST|Techniparts|Goed Gemerkt|TinyTronics|Onlinekabelshop|123 3D|123accu|Otronic|Opencircuit|Mouser Electronic|HOD Electronics|Conrad|Eleshop|Reichelt|RECORD SHOP",
+    "Tikkie": "Tikkie|Betaalverzoek",
+    "Auto": "Auto|parking|Verkeersboetes|Tankstation|Q8 AIRE|^OK |Esso|TotalEnergies|Total |P Centrum Breda|EXPRTILBURG|KEIZ3320|TMCP|GARAGE VAN OOL|Shell|TINQ|^BP|TAMOIL|abonnementsgelden ANWB|KAMPEN ASSURANTIE|VKG VERZEKERINGEN|MijnAutoOnderdelen|Interparking|ParkBee|Q-Park|QPark|79-KDL-1",
+    "Fiets": "Tweewieler|fiets",
+    "Huisdieren": "Pierre Beelen|VDierenspeciaalzaak|Dierenkliniek|Verhuisdieren|Doornakker EINDHOVEN",
+    "Woning": "BRABANT WATER NV|Gemeente Waalre|hypotheek|van Hest|Gwenny",
+    "Inboedel": "Meubella|Planten|Praxis|Matt Sleeps|ABC Webshop|Vloerglijders\.nl|groenovatie|Tuincentrum|Intratuin|TuincentrCoppelmans|Pimpernelzorgkw|vijver|Randijk Bamboe|VenetaBV|boom kappen",
+    "Zorg": "Zilveren Kruis|Infomedics|Apotheek|Al Dente|Pearle|R.A. van Hek|Specsavers",
+    "Verzekeringen overig": "FBTO|REAAL",
+    "Gas-water-licht": "ANWB Energie|^Vandebron|Waterschap de Dommel",
+    "Abonnementen": "Piano|PLINQ|DPG MEDIA|Simyo|MICROSOFT|Kosten OranjePakket|Kosten tweede rekeninghouder",
+    "Studie": "Stichting Eindhovens Christelijk Vo|Lara Baltessen Omschrijving: Collegegeld|Stichting Avans Omschrijving: Collegegeld|Van.*Dijk",
+    "Zakgeld/gift": "kleedgeld|studiegeld|zakgeld|Theoriecentrum|Rijschool",
+    "Kinderbijslag": "KINDERBIJSLAG|VOORSCHOT KIT/Kgb",
+    "Belasting correctie": "Belastingdienst Apeldoorn|BELASTINGDIENST Omschrijving: 0147312312606013",
+    "Vakantie-uitjes": "Viv.*s.*Kitchen|MENEER DE BOER|PULLENS HELVOIRT|Gamified|VENLO|MAASTRICHT|Arcen|GRUBBENVORST|MENNEKE ETEN|ChinInd SingAn|HistOpenluchtmuseum|QUEPASA|CCVT Bello|ZTLMaasakkersHoving|CCVQUEEN|ROMA ITA|Tiqets|AIRPORT|STARBUCKS|THE HAPPINESS CAF|Trafalgar Pub|DTGM standhouders via Stichting GoCredible|SUPER U|STATION U|PIACENZA|Cupola Basilica|SAIS SNACK|BAUME|DIJON|BRICO MARCH|KIABI DOLE|CIAMPINO|39ORGELET|39CLAIRVAUX|FOURNIL DE MANON|BELLECIN|FONTAINE|VALENCE|MERCANTINE|RUOMS|LABEAUME|BARJAC|BALAZUC|DAGNEUX|ST RAMBERT|BIJOUX 34PALAVAS|VALLON PONT|ARDECHE|CCVCarrousel|Het Rondje SGRAVENHAGE|Otravo BV|TAPO TAPONAS|BELLAS ICE|IJssalo|Strandtent|Mc.*Donald|IKEK|bar exploitat|CCVCB Leisure|Waldhoorn BV OTTERLO|theater|Happy Italy|De oude telefooncentra|HappyFood|Locomotion Diner|SOS Sport en Events|Lattes.*Literature|Restaurant|RISTORANTE|Friet|Quan Nguyen|La Providence|SEPt Oude Wandelpark|Efteling|Dunkin|BROWNIES EN DOWNIE|Rest.*Zoet.*Zout|Strandpaviljoen|Brasserie|BIKMIK MARKT|Afghani|HEZEMANS|PAYMagieMaastricht|At the Movies|ZettleMaasakkersHov|ECOLONIE|Koala Express|BROOD2DAY|Brandweerkantin|Horeca|Pathe|Ijsselhallen|Chidoz|LOODS 61|SMAAKVOL|Bedrijfsrest|Sauna|Cafe|Next Nature Network|A'DAM Six Senses|Sherlocked|Bowling|Plein 4|IJsco Fantastico|De Vooruitgang|Avocado Show|Time4t|Hotel|tourist|Stayokay|Conscious Westerpark|Moon AMSTERDAM|Majestic Amsterdam|Spa Sense|Subway|The Happiness Kitchen|Fifth NRE|CCVSancy Tres|waterreus|t Goude Hooft|Belicio Cheats|Soil Of Amsterdam|Kottmann en AMSTERDAM|alpenpark|Snowworld|Outside Escape|TicketCounter|Cafetaria|STATION OUTDOOR RETIE|INCASSO CREDITCARD|Huttopia|Roompot|Parkhoeve de Middelt|CCVBar Potential|CAMP BEAUREGARD|A J M de Bruijn|Bolier|Erik|Paasen|Venbrux|AGG JANSSEN|Saskia",
+    "Giften": "Greenpeace|Stichting Universiteitsf|Mantelzorgelijk|Rop",
     "Pinnen": "Geldmaat|RABO SALLAND",
-    "Onbekend": "CCVBP|NEUAIRserv|Kuipers DEVENTER|BOERSMA APELDOORN|^STG MOLLIE PAYMENTS|BOMA4610|Global Collect|PPRO PAYMENT|TMCPVALKENSWAARD|Pin Pont",
+    "Onbekend": "Bayeuxlaan|Payments via Adyen|CCVErgon|Verkadef|Butlaroo Services|CCVKroonenberg|CCVElektrotech Studie|BRAINPTBackWERK|PARK7520|CCVBP|NEUAIRserv|Kuipers DEVENTER|BOERSMA APELDOORN|^STG MOLLIE PAYMENTS|BOMA4610|Global Collect|PPRO PAYMENT|TMCPVALKENSWAARD|Pin Pont",
 }
 
-category_to_ignore = "Woonwenz|Philips|ING Bank|STICHTING FONTYS|Annakloos|Stichting Bitva|BTC Direct Euro|Medipoint|Hockey|Bakx|L\.H\. Baltessen"
+rows_to_ignore = "Woonwenz|Philips|ING Bank|Fontys|Annakloos|Stichting Bitva|BTC Direct Euro|Medipoint|Hockey|Maaltijdservice|Overboeking|Bakx|L\.H\. Baltessen"
 
 # Initialize selected_rows to empty DataFrames for each row_name
 category_rows = {category_name: pd.DataFrame() for category_name, _ in categories_to_select.items()}
@@ -78,9 +81,9 @@ for _, category_data in category_rows.items():
     selected_indexes = selected_indexes.union(category_data.index)
 unhandled_rows_df = df.drop(selected_indexes)
 
-# Exclude rows matching the regular expressions in categories_to_select
-for _, category_data in category_rows.items():
-    unhandled_rows_df = unhandled_rows_df[~unhandled_rows_df['Naam / Omschrijving'].str.contains(category_to_ignore, case=False, regex=True)]
+# Exclude rows matching the regular expression in rows_to_ignore
+for column_name in columns_to_check:
+    unhandled_rows_df = unhandled_rows_df[~unhandled_rows_df[column_name].str.contains(rows_to_ignore, case=False, regex=True)]
 
 total_unhandled_amount = 0.0
 if not unhandled_rows_df.empty:
